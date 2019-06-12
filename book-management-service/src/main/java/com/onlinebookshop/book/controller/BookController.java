eb0ebb4c8bb2c1ce7b85da1e6766aa7c1a825fa4
KiionCC <136438673@qq.com>
2019-06-12 16:37:49 +0800

    update book management

--- book-management-service/src/main/java/com/onlinebookshop/book/controller/BookController.java
+++ book-management-service/src/main/java/com/onlinebookshop/book/controller/BookController.java
@@ -13,28 +13,55 @@ public class BookController {
     @Autowired
     BookMapper bookMapper;
 
+    //所有书籍
+    @RequestMapping(path = "/allBook",method = RequestMethod.GET)
+    public ResponseEntity getAllBook()
+    {
+        List<Book> Books = bookMapper.getAllBook();
+        return ResponseEntity.ok(Books);
+    }
+
     //书籍列表
     @RequestMapping(path = "/bookList",method = RequestMethod.GET)
-    public ResponseEntity getList()
+    public ResponseEntity getBookList(@RequestParam(value = "pageNum",required = false)String pageNum, @RequestParam(value = "pageSize",required = false)String pageSize)
     {
-        List<Book> Books = bookMapper.getBookList();
+        if(pageNum == null)
+            pageNum = "1";
+        if(pageSize == null)
+            pageSize = "10";
+
+        int skip = (Integer.parseInt(pageNum) - 1) * Integer.parseInt(pageSize);
+        List<Book> Books = bookMapper.getBookList(skip, Integer.parseInt(pageSize));
         return ResponseEntity.ok(Books);
     }
 
     //按id查看
     @RequestMapping(path = "/book",method = RequestMethod.GET)
-    public ResponseEntity getBook(@RequestParam(value = "book_id",required = false)int book_id)
+    public ResponseEntity getBook(@RequestParam(value = "book_id",required = false)String book_id)
     {
-        List<Book> Books = bookMapper.getBook(book_id);
-        return ResponseEntity.ok(Books);
+        if(book_id == null)
+            return null;
+        else{
+            List<Book> Books = bookMapper.getBook(Integer.parseInt(book_id));
+            return ResponseEntity.ok(Books);
+        }
+
     }
 
     //搜索
     @RequestMapping(path = "/search",method = RequestMethod.GET)
-    public ResponseEntity searchBook(@RequestParam(value = "key",required = false)String key)
+    public ResponseEntity searchBook(@RequestParam(value = "key",required = false)String key, @RequestParam(value = "pageNum",required = false)String pageNum, @RequestParam(value = "pageSize",required = false)String pageSize)
     {
+        if(key == null)
+            key = "%";
+        if(pageNum == null)
+            pageNum = "1";
+        if(pageSize == null)
+            pageSize = "10";
+
+        int skip = (Integer.parseInt(pageNum) - 1) * Integer.parseInt(pageSize);
         String complete = "%" + key + "%";
-        List<Book> Books = bookMapper.searchBook(complete);
+        List<Book> Books = bookMapper.searchBook(complete, skip, Integer.parseInt(pageSize));
         return ResponseEntity.ok(Books);
     }
 
--- book-management-service/src/main/java/com/onlinebookshop/book/mapper/BookMapper.java
+++ book-management-service/src/main/java/com/onlinebookshop/book/mapper/BookMapper.java
@@ -9,15 +9,19 @@ import java.util.List;
 @Mapper
 @Repository
 public interface BookMapper {
-    //查看书籍列表
+    //查看所有书籍
     @Select("select * from book")
-    List<Book> getBookList();
+    List<Book> getAllBook();
+
+    //查看书籍列表
+    @Select("select * from book limit #{skip},#{pageSize}")
+    List<Book> getBookList(@Param(value = "skip")int skip, @Param(value = "pageSize")int pageSize);
 
     //按id查看书籍
     @Select("select * from book where book_id=#{book_id}")
     List<Book> getBook(@Param(value = "book_id")int book_id);
 
     //搜索书籍
-    @Select("select * from book where title like #{key}")
-    List<Book> searchBook(@Param(value = "key")String key);
+    @Select("select * from book where title like #{key} limit #{skip},#{pageSize}")
+    List<Book> searchBook(@Param(value = "key")String key, @Param(value = "skip")int skip, @Param(value = "pageSize")int pageSize);
 }
